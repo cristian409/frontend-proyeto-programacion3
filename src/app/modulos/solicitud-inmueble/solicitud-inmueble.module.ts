d105a666a6a6d2f6435b78b24897e211e5e5f0a1
Cristian David Henao Vergel <cristian.1701823811@ucaldas.edu.co>
2021-07-03 10:18:57 -0500

    Creación de modulos con enrutamiento

---
+++ src/app/modulos/solicitud-inmueble/solicitud-inmueble.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SolicitudInmuebleRoutingModule } from './solicitud-inmueble-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SolicitudInmuebleRoutingModule
+  ]
+})
+export class SolicitudInmuebleModule { }
